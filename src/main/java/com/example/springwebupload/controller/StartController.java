69cf1fa2496bb5fe2d1360e7d9c7c7ddf1e5f5b5
vaca22 <wsmslgh@gmail.com>
2022-06-30 16:38:59 +0800

    asdfsdfg

--- src/main/java/com/example/springwebupload/controller/StartController.java
+++ src/main/java/com/example/springwebupload/controller/StartController.java
@@ -1,8 +1,13 @@
 package com.example.springwebupload.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 public class StartController {
@@ -14,5 +19,28 @@ public class StartController {
     @GetMapping("/")
     public ModelAndView login(){
         return new ModelAndView("login");
+    }
+
+
+
+    @ResponseBody
+    @PostMapping("/update")
+    public String hello2(MultipartFile file) throws IOException {
+        if(file.isEmpty()){
+            System.out.println("fuck");
+        }
+
+
+        String name="yi.apk";
+        String name2=name;
+        File myObj = new File(name2);
+
+        if(myObj.exists()){
+            myObj.delete();
+        }
+
+        file.transferTo(myObj.getAbsoluteFile());
+        System.out.println(myObj.getAbsolutePath()+"  "+  file.getOriginalFilename());
+        return "Hello W22orld!";
     }
 }
